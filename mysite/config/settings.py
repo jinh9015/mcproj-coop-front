e31b7d7a2278427b82d0201743a59710d87984b7
blanchedal <haprk83@gmail.com>
2023-05-24 11:36:17 +0900

    add-host

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 's!e!su8fn9ydb4m7hbkm07*d6y_@5ljhge$uiz)qx@b=3nm95m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
