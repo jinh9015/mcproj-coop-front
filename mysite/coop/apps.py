424f32c07436c005271ccacbb8a6284f62104caa
U-leee <kkang104@example.com>
2023-05-25 02:26:05 +0900

    delete mysite

--- mysite/coop/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoopConfig(AppConfig):
-    name = 'coop'
--- mysite/kakaomap/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return render(request,'kakaomap/map.html')
-
-# def locate(request):
-#     return render(request,'coop/coop_locate.html')
