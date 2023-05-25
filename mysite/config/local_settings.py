189a2892f89144a13b469897a00298117589d5e9
jinh9015 <jinh9015@gmail.com>
2023-05-25 01:15:22 +0000

    MySQL edit

--- mysite/config/local_settings.py
+++ mysite/config/local_settings.py
@@ -1,12 +1,23 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'choe', # DB이름
-        'USER': 'root', # DB 유저 아이디
-        'PASSWORD': 'ssd78603183', # 비밀번호
-        'HOST': 'localhost', # 또는 자신이 설정한 호스트
-        'PORT': '3306', # db가 연결된 포트(여기서는 기본 포트)
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'choe', # DB이름
+#        'USER': 'root', # DB 유저 아이디
+#        'PASSWORD': 'ssd78603183', # 비밀번호
+#        'HOST': 'localhost', # 또는 자신이 설정한 호스트
+#        'PORT': '3306', # db가 연결된 포트(여기서는 기본 포트)
+#    }
+#}
 
 SECRET_KEY = 's!e!su8fn9ydb4m7hbkm07*d6y_@5ljhge$uiz)qx@b=3nm95m'
+
+DATABASES = {
+    ‘default’: {
+        ‘ENGINE’: ‘django.db.backends.mysql’,
+        ‘NAME’: ‘coop-mysql’,
+        ‘USER’: ‘root’,
+        ‘PASSWORD’: ‘password’,
+        ‘HOST’: ‘43.200.10.203’,
+        ‘PORT’: ‘3307’,
+    }
+}
